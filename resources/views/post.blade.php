e610924058f2463d0d35bca196757b03039de0f0
Dhiya Ulhaq Ramadhanty <58680899+dhiyaulhaqr@users.noreply.github.com>
2021-10-20 15:04:33 +0700

    menambahkan post category & eloquent relationship pada coba-laravel

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,6 +4,8 @@
 <article>
     <h1 class="mb-5">{{ $post->title }}</h1>
     
+
+    <p>by. dhiya ulhaqr in <a href="/categories/{{ $post->category->slug}}">{{$post->category->name}} </p></a>
     {!! $post->body !!}
 </article>
 
