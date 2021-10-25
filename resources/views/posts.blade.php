75933c2b066cec8b4a050588f4f39deebb290571
Dhiya Ulhaq Ramadhanty <58680899+dhiyaulhaqr@users.noreply.github.com>
2021-10-25 22:25:00 +0700

    N+1 problem pada aplikasi coba-laravel

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,7 @@
 @extends('layouts.main')
 
 @section('container')
-  <h1 class="mb-5">Halaman Blog Posts</h1>
+  <h1 class="mb-5">{{ $title }}</h1>
    
 
 @foreach ( $posts as $post )
@@ -11,6 +11,7 @@
      <a href="/posts/{{ $post->slug }}" class="text-decoration-none">
      {{ $post->title }}</a>
     </h2>
+
     <p>by. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug}}"class="text-decoration-none">{{$post->category->name}} </p></a>
 
    <p>{{ $post->excerpt }}</p>
