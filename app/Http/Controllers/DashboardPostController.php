323eccd88ca3437975133cd984fb262860beefe9
Dhiya Ulhaq Ramadhanty <58680899+dhiyaulhaqr@users.noreply.github.com>
2021-11-25 01:39:54 +0700

    Update & Delete Post pada laravel

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -81,7 +81,10 @@ class DashboardPostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        return view('dashboard.posts.edit', [
+            'post' => $post,
+            'categories' => Category::all()
+        ]);
     }
 
     /**
@@ -93,7 +96,25 @@ class DashboardPostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-        //
+        $rules = [
+            'title' => 'required|max::255',
+            'category_id' => 'required',
+            'body' => 'required'
+        ];
+        if($request->slug != $post->slug) {
+            $rules['slug'] = 'required|unique:posts';
+        }
+
+        $validateData = $request->validate($rules);
+
+
+        $validateData['user_id'] = auth()->user()->id;
+        $validateData['excerpt'] = Str::limit(strip_tags($request->body),200);
+
+        Post::where('id', $post->id)
+        ->update($validateData);
+
+        return redirect('/dashboard/posts')->with('success', 'Post has been updated!');
     }
 
     /**
@@ -104,7 +125,9 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        Post::destroy($post->id);
+
+        return redirect('/dashboard/posts')->with('success', 'Post has been deleted');
     }
 
     public function checkSlug(Request $request)
--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -8,9 +8,16 @@
         <h1 class="mb-3">{{ $post->title }}</h1>
 
 
-        <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span>Back to all myposts</a>
-        <a href="" class="btn btn-warning"><span data-feather="edit"></span>edit</a>
-        <a href="" class="btn btn-danger"><span data-feather="x-circle"></span>delete</a>
+        <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span>Back to all my posts</a>
+
+        <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit"></span>Edit</a>
+        
+        <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                      @method('delete')
+                      @csrf
+                      <button class="btn btn-danger" onclick="return confirm('Are you sure?')"><span data-feather="x-circle"></span>Delete
+                    </button>
+                    </form>
 
         <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="imh-fluid mt-3">
 
